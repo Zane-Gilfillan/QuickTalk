ae2c5207f77505e011ba44847fd323e81cc48b34
Zane-Gilfillan <zane.gilfillan@gmail.com>
2022-06-06 15:38:20 -0500

    more inits

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Header() {
+  return <HeaderContainer>
+
+            <h1>test test</h1>
+
+        </HeaderContainer>
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+    color: 'red';
+`
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB3sGvuY8Io0nR9aETNgC7z8V65twDzzGg",
+    authDomain: "quicktalk-9c297.firebaseapp.com",
+    projectId: "quicktalk-9c297",
+    storageBucket: "quicktalk-9c297.appspot.com",
+    messagingSenderId: "926620966496",
+    appId: "1:926620966496:web:9aa000ce15cba3f95e6d05"
+};
